bd3e628656d6bc727552a1f103765053d246e33b
polupanoff <polupanoff@gmail.com>
2024-03-03 13:41:12 +0500

    add menu for form hidden field

---
+++ lib/form_hidden_field_menu.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:layout_editor/menu.dart';
+import 'package:layout_editor/component_table.dart';
+import 'package:layout_editor/item.dart';
+import 'package:layout_editor/page.dart';
+import 'package:layout_editor/root.dart';
+
+class FormHiddenFieldMenu extends ComponentAndSourceMenu {
+  FormHiddenFieldMenu(super.layoutModel, super.target, {super.onChanged});
+
+  @override
+  List<PopupMenuEntry<Item>> getComponentMenu(void Function(Item)? onChanged) {
+    return [
+      PopupMenuItem(
+        child: Text("Удалить скрытое поле"),
+        onTap: () {
+          layoutModel.deleteItem(target);
+          onChanged!(target);
+        },
+      )
+    ];
+  }
+}
