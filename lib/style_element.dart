96f7e252e9ad72e63313145c11dee85fb5e3bccf
polupanoff <polupanoff@gmail.com>
2024-03-23 15:09:39 +0500

    add style properties

--- lib/style_element.dart
+++ lib/style_element.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:layout_editor/item.dart';
 import 'package:layout_editor/property.dart';
-import 'package:layout_editor/source.dart';
+import 'package:layout_editor/style.dart';
 
-class StyleElement extends LayoutSource {
+class StyleElement extends LayoutStyle {
   StyleElement(name) : super("styleElement", name) {
     properties["color"] = Property("цвет", Colors.blue, type: Color);
+    properties["backgroundColor"] = Property("цвет фона", Colors.blue, type: Color);
+    properties["alignment"] =
+        Property("выравнивание", Alignment.centerLeft, type: Alignment);
+    properties["fontSize"] = Property("размер шрифта", 11, type: int);
+    properties["fontWeight"] = Property("насыщенность шрифта", FontWeight.normal, type: FontWeight);
   }
 
 }
