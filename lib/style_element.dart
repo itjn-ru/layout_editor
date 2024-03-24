16ef382a0f4430e67a86bd9a187e3f50f4d0b0b3
polupanoff <polupanoff@gmail.com>
2024-03-24 17:09:58 +0500

    change default background color

--- lib/style_element.dart
+++ lib/style_element.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:layout_editor/property.dart';
 import 'package:layout_editor/style.dart';
+import 'package:uuid_type/uuid_type.dart';
 
 class StyleElement extends LayoutStyle {
   StyleElement(name) : super("styleElement", name) {
-    properties["color"] = Property("цвет", Colors.black, type: Color);
-    properties["backgroundColor"] = Property("цвет фона", Colors.blue, type: Color);
-    properties["alignment"] =
+    properties['id'] =
+        Property("идентификатор", Uuid.parse(uuid.v4()), type: Uuid);
+    properties['color'] = Property("цвет", Colors.black, type: Color);
+    properties['backgroundColor'] = Property("цвет фона", Colors.transparent, type: Color);
+    properties['alignment'] =
         Property("выравнивание", Alignment.centerLeft, type: Alignment);
-    properties["fontSize"] = Property("размер шрифта", 11, type: double);
-    properties["fontWeight"] = Property("насыщенность шрифта", FontWeight.normal, type: FontWeight);
+    properties['fontSize'] = Property("размер шрифта", 11, type: double);
+    properties['fontWeight'] = Property("насыщенность шрифта", FontWeight.normal, type: FontWeight);
   }
 
 }
