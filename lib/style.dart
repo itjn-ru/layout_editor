42743f49315425462d271ff7051660221d6bdf9b
polupanoff <polupanoff@gmail.com>
2024-03-24 17:09:19 +0500

    add style class

--- lib/style.dart
+++ lib/style.dart
@@ -1,11 +1,24 @@
-import 'package:layout_editor/property.dart';
-import 'package:uuid/uuid.dart';
+import 'package:uuid_type/uuid_type.dart';
 import 'component_and_source.dart';
 
 class LayoutStyle extends LayoutComponentAndSource {
-  LayoutStyle(super.type, super.name) {
-    properties["id"] = Property("идентификатор", const Uuid().v4(), type: Uuid);
-  }
+  LayoutStyle(super.type, super.name);
 }
 
-class Style{}
+class Style {
+  Uuid id;
+  String name;
+
+  static Style basic = Style(Uuid.nil, "базовый стиль");
+
+  Style(this.id, this.name);
+
+
+  @override
+  bool operator == (Object other) =>
+      other is Style && id == other.id;
+
+  @override
+  int get hashCode => Object.hash(id, name);
+
+}
