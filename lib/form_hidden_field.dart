ca1765663e7a08f5de7f751423b01d25bd28dfdd
polupanoff <polupanoff@gmail.com>
2024-03-24 17:07:05 +0500

    change lib uuid to uuid_type

--- lib/form_hidden_field.dart
+++ lib/form_hidden_field.dart
@@ -1,15 +1,12 @@
 import 'package:layout_editor/component.dart';
 import 'package:layout_editor/property.dart';
-import 'package:uuid/uuid.dart';
-
+import 'package:uuid_type/uuid_type.dart';
 
 class FormHiddenField extends LayoutComponent {
   FormHiddenField(name) : super("hiddenField", name) {
     properties["caption"] = Property("подпись", "");
-    properties["id"] = Property("идентификатор", const Uuid().v4(), type: Uuid);
+    properties["id"] =
+        Property("идентификатор", Uuid.parse(uuid.v4()), type: Uuid);
     properties["source"] = Property("источник", "");
   }
-
-
-
 }
